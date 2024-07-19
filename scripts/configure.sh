8073bd0b147e7bef01d550f2511fc534f11f1130
imranismail <hey@imranismail.dev>
2024-07-20 02:20:35 +1200

    improve: logs

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -27,20 +27,62 @@ install() {
 
   OVERSEERR_TEMPLATE=$(jq -s 'reduce .[] as $x ({}; . * $x)' overseerr/settings.json overseerr/settings.json.partial)
 
+  if [ "$ACTION" == "--dry-run" ]; then
+    echo ""
+    echo "🟢 Would generate overseerr/settings.json"
+    echo ""
+  fi
+
   sed -e "s/\${RADARR_API_KEY}/$RADARR_API_KEY/g" \
       -e "s/\${SERIES_SONARR_API_KEY}/$SERIES_SONARR_API_KEY/g" \
       -e "s/\${ANIME_SONARR_API_KEY}/$ANIME_SONARR_API_KEY/g" \
       -e "s/\${PROWLARR_API_KEY}/$PROWLARR_API_KEY/g" \
       <<< "$OVERSEERR_TEMPLATE" > "$OVERSEERR_OUTPUT"
 
+  if [ "$ACTION" == "--dry-run" ]; then
+    echo ""
+    echo "🟢 Would generate radarr/config.xml"
+    echo ""
+  fi
+
   sed -e "s/\${RADARR_API_KEY}/$RADARR_API_KEY/g" \
     < radarr/config.xml.template > "$RADARR_OUTPUT"
+
+  if [ "$ACTION" == "--dry-run" ]; then
+    echo ""
+    echo ""
+    echo "🟢 Would generate series-sonarr/config.xml"
+    echo ""
+  fi
+
   sed -e "s/\${SERIES_SONARR_API_KEY}/$SERIES_SONARR_API_KEY/g" \
     < series-sonarr/config.xml.template > "$SERIES_SONARR_OUTPUT"
+
+  if [ "$ACTION" == "--dry-run" ]; then
+    echo ""
+    echo ""
+    echo "🟢 Would generate anime-sonarr/config.xml"
+    echo ""
+  fi
+
   sed -e "s/\${ANIME_SONARR_API_KEY}/$ANIME_SONARR_API_KEY/g" \
     < anime-sonarr/config.xml.template > "$ANIME_SONARR_OUTPUT"
+
+  if [ "$ACTION" == "--dry-run" ]; then
+    echo ""
+    echo ""
+    echo "🟢 Would generate prowlarr/config.xml"
+    echo ""
+  fi
+
   sed -e "s/\${PROWLARR_API_KEY}/$PROWLARR_API_KEY/g" \
     < prowlarr/config.xml.template > "$PROWLARR_OUTPUT"
+
+  if [ "$ACTION" == "--dry-run" ]; then
+    echo ""
+    echo ""
+    echo "🟢 Config generated successfully"
+  fi
 }
 
 install "$@"
